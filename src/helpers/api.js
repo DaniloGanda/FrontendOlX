fc6b1171c1dca48397c7bc5d4857da3e8db0bdc6
Danilo Ganda <daniloganda95@gmail.com>
2021-04-06 22:35:05 -0300

    Adicionado o projeto

---
+++ src/helpers/api.js
@@ -0,0 +1,68 @@
+import Cookies from 'js-cookie' // tem baixar a depencia js-cookie --save
+import qs from 'qs'; // tem q baixar a depencia qs --save
+
+const BASEAPI = 'http://alunos.b7web.com.br:501';
+
+// requisicao post
+const apiFetchPost = async (url, body) =>{
+
+    if(!body.token){
+        let token = Cookies.get('token');
+        if(token){
+            body.token = token;
+        }
+    }
+
+    const res = await fetch(BASEAPI+url, {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const json = await res.json();
+
+    if(json.notallowed){
+        window.location.href='/login';
+        return;
+    }
+
+    return json;
+}
+
+//requisicao get
+const apiFetchGET = async (url, body = []) =>{
+
+    if(!body.token){
+        let token = Cookies.get('token');
+        if(token){
+            body.token = token;
+        }
+    }
+
+    const res = await fetch(`${BASEAPI+url}?${qs.stringify(body)}`);
+
+    const json = await res.json();
+
+    if(json.notallowed){
+        window.location.href='/login';
+        return;
+    }
+
+    return json;
+}
+
+//
+const api = {
+    login:async (email, password) =>{
+        const json = await apiFetchPost(
+            '/user/signin', 
+            {email, password}
+        );
+        return json;
+    }
+}
+
+export default () => api;
